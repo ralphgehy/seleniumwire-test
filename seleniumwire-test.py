7a466eef6a054495288085dafa6ec67239e89db9
Ralph Gehy <ralph.gehy@hudl.com>
2023-05-24 13:08:41 -0400

    updated seleniumwire file

--- seleniumwire-test.py
+++ seleniumwire-test.py
@@ -1,22 +1,32 @@
 import time
-# from seleniumwire import webdriver
-from selenium import webdriver
+import boto3
+from seleniumwire import webdriver
+# from selenium import webdriver
 from selenium.webdriver.common.by import By
+from seleniumwire.webdriver import DesiredCapabilities
 from webdriver_manager.chrome import ChromeDriverManager
 chrome_options = webdriver.ChromeOptions()
 # you are going to tell selenium the IP where the code will run
 # replace scrap with the IP, I use scrap because my docker-compose file uses that name
-chrome_options.add_argument('--proxy-server=0.0.0.0:8087')
+chrome_options.add_argument('--proxy-server=3.83.190.70:8087')
 chrome_options.add_argument('--ignore-certificate-errors')
 
 # driver = webdriver.Chrome(ChromeDriverManager().install())
 options = {
     'auto_config': False,
-    'addr': '0.0.0.0',
+    'addr': '3.83.190.70',
     'port': 8087
 }
+devicefarm_client = boto3.client("devicefarm")
+
+testgrid_url_response = devicefarm_client.create_test_grid_url(
+      projectArn="arn:aws:devicefarm:us-west-2:810639563432:project:f86810bd-a3a1-47f7-bdf3-7127a8b9f211",
+      expiresInSeconds=300
+      )
+
+
 driver = webdriver.Remote(
-    command_executor="http://0.0.0.0:4444/wd/hub",
+    command_executor=testgrid_url_response,
     desired_capabilities=chrome_options.to_capabilities(),
     seleniumwire_options=options
 )
